c1656100315eecccce18355d1403eb0311b1017a
aloro <alorotams@gmail.com>
2025-02-13 09:54:19 -0500

    First test

--- src/Main.java
+++ src/Main.java
@@ -8,18 +8,6 @@ public class Main {
         int difference = calculator.subtract(10, 5);
         System.out.println(difference);
 
-
-        /*
-        System.out.println("Hello World!"); //like console.log
-        Greeting greeting = new Greeting(); //Create instance (object) of the Greeting class (class name & the object creation must match)
-        greeting.hello(); // combines the two class using their methods
-
-
-        Messenger messenger = new Messenger();
-        messenger.sendMessage();
-
-         */
-
         //Variables
         String greeting = "Hello World!";
         int number = 5;
